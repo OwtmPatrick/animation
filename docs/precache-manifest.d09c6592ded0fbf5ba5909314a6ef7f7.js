1c55ef3afaee949be5c2fc5e2a49872edf4fc8ad
owtmpatrick <owtmpatrick@gmail.com>
2020-02-19 00:05:44 +0500

    added docs folder

---
+++ docs/precache-manifest.d09c6592ded0fbf5ba5909314a6ef7f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b2070b2a4f73a317e22799817042e2",
+    "url": "/animation/index.html"
+  },
+  {
+    "revision": "6e58bd07a2c3986d5868",
+    "url": "/animation/static/css/main.e6ec940d.chunk.css"
+  },
+  {
+    "revision": "920a572fa7042fe3c3a1",
+    "url": "/animation/static/js/2.755b13ef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/animation/static/js/2.755b13ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e58bd07a2c3986d5868",
+    "url": "/animation/static/js/main.490506c5.chunk.js"
+  },
+  {
+    "revision": "b310bd844e584bca0531",
+    "url": "/animation/static/js/runtime-main.3961bc0a.js"
+  }
+]);
